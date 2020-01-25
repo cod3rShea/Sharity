6b43852923701dd7618673195875055d7861848c
Sean Michael Shea <20631582+cod3rShea@users.noreply.github.com>
2020-01-25 12:40:45 -0500

    after register user go to register resteruant

--- config/connection.js
+++ config/connection.js
@@ -8,8 +8,8 @@ if (process.env.JAWSDB_URL) {
         host: 'localhost',
         port: 3306,
         user: "root",
-        password: "root",
-        database: "sharity_db"
+        password: "root1234",
+        database: "sharity"
     });
     
 }
