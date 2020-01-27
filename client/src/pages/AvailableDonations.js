10e168734b0ae782b17a02161421d6c67ad58fbb
amw157 <amw157@case.edu>
2020-01-26 23:13:55 -0500

    update map functionality

--- client/src/pages/AvailableDonations.js
+++ client/src/pages/AvailableDonations.js
@@ -1,16 +1,18 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import Card from "../components/Card/index";
+import CardHolder from "../components/CardHolder/index"
+import ClaimBtn from "../components/ClaimButton/index"
 import axios from "axios"
 
 
 class AvailableDonations extends Component {
     state = {
-        // foodItemDescription: '',
-        // amount: '',
-        // donationComments: '',
-        // storageReqs: '',
-        // isClaimed: ''
         donationSubmission: []
+            // foodItemDescription,
+            // amount,
+            // donationComments,
+            // storageReqs,
+            // isClaimed
     }
 
     componentDidMount() {
@@ -20,21 +22,66 @@ class AvailableDonations extends Component {
             }
         })
             .then(res => {
-                console.log(res.data[0]);
-                console.log(res.data[0].foodItemDescription);
+                // console.log(res.data[0]);
+                // console.log(res.data[0].foodItemDescription);
 
-                this.setState({
-                   donationSubmission:[
+                this.setState(
+                    this.donationSubmission = [
                         res.data[0].foodItemDescription,
                         res.data[0].amount,
                         res.data[0].donationComments,
                         res.data[0].storageReqs,
-                        res.data[0].isClaimed
-                   ]})
+                        res.data[0].isClaimed,
+                        res.data[0].id
+                    ]
+                )
+                // console.log(this.donationSubmission);
+                this.CreateCard();
             })
-        .catch(function (error) {
-            console.log(error);
-        });
+            .catch(function (error) {
+                console.log(error);
+            });
+        
+    }
+
+    CreateCard() {
+        console.log(this.donationSubmission)
+        // const donationSubmission = this.donationSubmission;
+        // const info = donationSubmission.map((donationSubmission) =>
+        //     <li key={donationSubmission.id}>{donationSubmission}</li>
+        // );
+        // return (
+        //     <ul>{info}</ul>
+        // )
+        const submission = this.donationSubmission
+
+        return (
+            <div>
+                <div>
+                    <p>test</p>
+                </div>
+                <div>
+                    <ul>
+                        {submission.map(item => (
+                            <li key={item.id}>
+                                {item.foodItemDescription}
+                            </li>
+                        ))}
+                    </ul>
+                
+                </div>
+            </div>
+        )
+
+        // this.donationSubmission.map(({donationSubmission}) =>
+        //     <Card >
+        //         <p>Title</p>
+        //         <div>{donationSubmission.foodItemDescription} {donationSubmission.amount} 
+        //             {donationSubmission.donationComments} {donationSubmission.storageReqs} 
+        //             {donationSubmission.isClaimed}
+        //         </div>
+        //         <ClaimBtn ClaimDonation={this.ClaimDonation} ></ClaimBtn>
+        //     </Card>)
     }
 
     ClaimDonation = event => {
@@ -53,16 +100,15 @@ class AvailableDonations extends Component {
     render() {
         return (
             <div>
-                <h1>Claim a Donation</h1>
-                <p>Take a look at the local donations and claim a donation you would like to pick up.</p>
-                <Card ClaimDonation={this.ClaimDonation}
-                // {this.state.donations.map((donation) => {
-                //     return <div>{donation.foodItemDescription} {donation.amount} {donation.donationComments}
-                // {donation.storageReqs} {donation.isClaimed}</div>
-                // })}
-                ></Card>
+                <div>
+                    <h1>Claim a Donation</h1>
+                    <p>Take a look at the local donations and claim a donation you would like to pick up.</p>
+                </div>
+                <div>
+                    <CardHolder {...this.CreateCard}></CardHolder>
+                </div>
             </div>
-        );
+        )
     }
 
 }
